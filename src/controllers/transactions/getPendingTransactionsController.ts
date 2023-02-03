51766387d86db5434a7d178678f0d34e1aa650a9
asbenjamin <shammahbenjamin@gmail.com>
2023-02-03 13:10:52 +0300

    Refactor code - return txinfo in one object

--- src/controllers/transactions/getPendingTransactionsController.ts
+++ src/controllers/transactions/getPendingTransactionsController.ts
@@ -1,7 +1,6 @@
 import { ethers } from "ethers";
 import { Request, Response } from "express";
 import dotenv from "dotenv";
-import abiSample from "../../utils/abisample.json";
 
 // load the environment variables from the .env file
 dotenv.config({
@@ -18,48 +17,11 @@ export async function getPendingTransactions(req: Request, res: Response) {
   try {
     const transactions: any = [];
 
-    const erc20interface = new ethers.utils.Interface(abiSample);
-
-    let decode = (txHash: string, inputData: string) => {
-      // console.log(inputData)
-      try {
-        const txData = erc20interface.parseTransaction({
-          data: inputData,
-        });
-        // console.log("\n\n\n  Got one ", txData);
-        return txData;
-      } catch (error) {
-        return false;
-      }
-    };
-
     provider.on("pending", async (tx) => {
       const txtInfo = await provider.getTransaction(tx);
 
       if (txtInfo && txtInfo.to && txtInfo.data != "0x") {
-        // const contractAddress = txtInfo.to;
-        // const byteCode = await provider.getCode(contractAddress);
-        // const gas = txtInfo.gasLimit.toNumber();
-        const gasPrice = txtInfo.gasPrice?.toNumber();
-        // const maxFeePerGas = txtInfo.maxFeePerGas?.toNumber();
-        const decodedData = decode(txtInfo.hash, txtInfo.data);
-        console.log(decodedData);
-
-        if (decodedData && decodedData.name == "addLiquididty") {
-          console.log("Yeessssssssssssssss");
-        }
-        // console.log(decodedData)
-
-        // transactions.push({
-        //   transactionHash: txtInfo.hash,
-        //   toAddress: txtInfo.to,
-        //   byteCode: byteCode,
-        //   gas: gas,
-        //   gasPrice: gasPrice,
-        //   maxFeePerGas: maxFeePerGas,
-        //   decoded: decodedData,
-        // });
-        transactions.push({ txtInfo, gasPrice, decodedData });
+        transactions.push({ txtInfo});
       }
     });
 
