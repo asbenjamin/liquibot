7f02ea4147181bc591737faa792fd6a1f1a6a33b
asbenjamin <shammahbenjamin@gmail.com>
2023-02-16 20:00:01 +0300

    Add placing of both buy and sell and error handling when data missing

--- src/utils/telegraf/botInit.ts
+++ src/utils/telegraf/botInit.ts
@@ -17,58 +17,72 @@ bot.start((ctx) => {
 
 bot.action("placeordereth", async (ctx) => {
   try {
-    ctx.reply(`Placing your order...`);
-    console.log("here we are, placing your order");
+    const orderSide = await redis.get("orderSide");
+    const orderType = await redis.get("orderType");
+    const qty = await redis.get("qty");
 
-    const orderData = {
-      symbol: "ETHUSDT",
-      side: await redis.get("orderSide"),
-      order_type: await redis.get("orderType"),
-      qty: Number(await redis.get("qty")),
-      time_in_force: "GoodTillCancel",
-      close_on_trigger: false,
-    };
+    if (orderSide && orderType && qty && Number(qty) > 0) {
+      ctx.reply(`Placing your order...`);
+      console.log("here we are, placing your order");
 
-    const lastTradedPrice = await bybitService.getLastTradedPrice("ETHUSDT");
-    let reduce_only = orderData.side === "Buy" ? false : true;
-    let price =
-      orderData.side === "Buy"
-        ? lastTradedPrice.lastTradedPrice - 0.05
-        : lastTradedPrice.lastTradedPrice + 0.05;
+      const orderData = {
+        symbol: "ETHUSDT",
+        side: orderSide,
+        order_type: orderType,
+        qty: Number(qty),
+        time_in_force: "GoodTillCancel",
+        close_on_trigger: false,
+      };
 
-    try {
-      let result;
-      if (orderData.order_type === "Market") {
-        result = await bybitService.placeOrder(
-          orderData.symbol,
-          orderData.side,
-          orderData.order_type,
-          orderData.qty,
-          orderData.time_in_force,
-          typeof price !== "undefined" ? price : undefined,
-          reduce_only,
-          orderData.close_on_trigger
-        );
-      } else
-        result = await bybitService.placeOrder(
-          orderData.symbol,
-          orderData.side,
-          orderData.order_type,
-          orderData.qty,
-          orderData.time_in_force,
-          price,
-          reduce_only,
-          orderData.close_on_trigger
-        );
-    } catch (err) {
-      console.error(err);
+      const lastTradedPrice = await bybitService.getLastTradedPrice("ETHUSDT");
+      let reduce_only = orderData.side === "Buy" ? false : true;
+      let price =
+        orderData.side === "Buy"
+          ? lastTradedPrice.lastTradedPrice - 0.05
+          : lastTradedPrice.lastTradedPrice + 0.05;
+
+      try {
+        let result;
+        if (orderData.order_type === "Market") {
+          result = await bybitService.placeOrder(
+            orderData.symbol,
+            orderData.side,
+            orderData.order_type,
+            orderData.qty,
+            orderData.time_in_force,
+            typeof price !== "undefined" ? price : undefined,
+            reduce_only,
+            orderData.close_on_trigger
+          );
+        } else
+          result = await bybitService.placeOrder(
+            orderData.symbol,
+            orderData.side,
+            orderData.order_type,
+            orderData.qty,
+            orderData.time_in_force,
+            price,
+            reduce_only,
+            orderData.close_on_trigger
+          );
+      } catch (err) {
+        console.error(err);
+      }
+
+      sendMessage(
+        Configs.bybit_bot_chat_id,
+        `A ${await redis.get("orderType")} ${await redis.get(
+          "orderSide"
+        )} order of ${await redis.get(
+          "qty"
+        )} ETHUSDT has been successfully placed`
+      );
+      await redis.flushall();
+    } else {
+      ctx.reply(
+        `Some order data is missing or invalid. Please set all order parameters before placing the order.`
+      );
     }
-
-    sendMessage(
-      Configs.bybit_bot_chat_id,
-      `Purchase order for ${orderData.qty} eth placed successfully`
-    );
-    await redis.flushall();
   } catch (error) {
     console.error(error);
     sendMessage(
@@ -104,14 +118,26 @@ bot.command("placeorder", (ctx) => {
   ctx.reply(
     `Select the order type, ${ctx.from.first_name}?`,
     Markup.inlineKeyboard([
-      Markup.button.callback("Place Limit Order", "Limit_Order"),
-      Markup.button.callback("Place Market Order", "Market_Order"),
+      Markup.button.callback("Place Limit Order", "limitOrder"),
+      Markup.button.callback("Place Market Order", "marketOrder"),
     ])
   );
 });
 
-bot.action("Limit_Order", async (ctx) => {
+bot.action("limitOrder", async (ctx) => {
   await redis.set("orderType", "Limit");
+
+  ctx.reply(
+    `Do you want to Buy or Sell`,
+    Markup.inlineKeyboard([
+      Markup.button.callback("🤑 Buy Some", "Buy"),
+      Markup.button.callback("💱 Sell Some", "Sell"),
+    ])
+  );
+});
+
+bot.action("marketOrder", async (ctx) => {
+  await redis.set("orderType", "Market");
 
   ctx.reply(
     `Do you want to Buy or Sell`,
@@ -127,11 +153,39 @@ bot.action("Buy", async (ctx) => {
 
   ctx.reply(
     `How much do you want`,
-    Markup.inlineKeyboard([Markup.button.callback("💱 Buy 5", "Buy_Five")])
+    Markup.inlineKeyboard([Markup.button.callback("💱 Buy 5", "buyFive")])
   );
 });
 
-bot.action("Buy_Five", async (ctx) => {
+bot.action("Sell", async (ctx) => {
+  await redis.set("orderSide", "Sell");
+
+  ctx.reply(
+    `How much do you want`,
+    Markup.inlineKeyboard([Markup.button.callback("💱 Sell 5", "sellFive")])
+  );
+});
+
+bot.action("buyFive", async (ctx) => {
+  await redis.set("qty", "5");
+
+  ctx.reply(
+    `Please confirm that you want to place a ${await redis.get(
+      "orderType"
+    )} ${await redis.get("orderSide")} order of ${await redis.get(
+      "qty"
+    )} ETHUSDT`,
+
+    Markup.inlineKeyboard([
+      Markup.button.callback("💱 Confirm Order", "placeordereth"),
+    ])
+  );
+  console.log(await redis.get("orderSide"));
+  console.log(await redis.get("orderType"));
+  console.log(await redis.get("qty"));
+});
+
+bot.action("sellFive", async (ctx) => {
   await redis.set("qty", "5");
 
   ctx.reply(
